88b60388e60780351f2c2e51dd5ae43c3a14a63f
Alex Ch <churkindev@gmail.com>
2021-11-09 13:58:55 +0300

    Google tests included

---
+++ mp2-lab3-stack-test/test_calculator.cpp
@@ -0,0 +1,13 @@
+#include "gtest.h"
+#include "..\mp2-lab3-stack\TCalculator.h"
+
+TEST(TCalculator, can_create_calculator)
+{
+	ASSERT_NO_THROW(TCalculator c);
+}
+
+//can_set_and_get_expression (можно записать строку с арифм. выражением и потом прочитать её ту же)
+//can_check_brackets (правильные скобки - true, неправильные - false)
+//can_convert_to_postfix
+//can_calculate (can_add, can_substract, can_multiply, can_divide, can_raise_to_the_power)
+//can_calculate_complex_expression (very_complex_with_brackets) ...
---
+++ mp2-lab3-stack-test/test_tstack.cpp
@@ -0,0 +1,17 @@
+#include "gtest.h"
+#include "..\mp2-lab3-stack\TStack.h"
+
+TEST(TStack, can_create_stack_with_positive_size)
+{
+	ASSERT_NO_THROW(TStack<int> s(100));
+}
+
+TEST(TStack, cant_create_stack_with_not_positive_size)
+{
+	ASSERT_ANY_THROW(TStack<int> s(-5));
+	ASSERT_ANY_THROW(TStack<int> s(0));
+}
+
+//не могу создать стек отрицательного размера
+//не могу извлечь из пустого стека
+//могу извлечь из стека (что те же самые элементы извлекаются)
